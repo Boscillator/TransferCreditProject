fd9821ff9ff7c33a3e3fdcc129dc825074befd9b
Fred Buchanan <oscillator.b@gmail.com>
2019-10-15 11:44:32 -0400

    Word2Vec Based algorithm.
    Wrote an algorithm based on averages of all the words in each document.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # TransferCreditProject
 ITWS Project.
+
+
+## Citations and Useful links
+1) https://www.shanelynn.ie/word-embeddings-in-python-with-spacy-and-gensim/
--- index.py
+++ index.py
@@ -2,43 +2,41 @@
 Indexes the database.
 Currently works off of CSV, will be converted to use the database directly.
 
-Requires nltk
-
+Requires spacy.
+This may not install correctly on windows, use WSL instead.
 ```
-pip install nltk
-python
->>> import nltk
->>> nltk.download('punkt')
->>> nltk.download('stopwords')
+pip install -u spacy
+python -m spacy download en_core_web_md
 ```
 """
 
 import csv
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
+import spacy
 
-stopwords = set(stopwords.words('english'))
 
-def jaccard_distance(a,b):
-    try:
-        return len(a - b)/len(a | b)
-    except ZeroDivisionError:
-        return 0.0
-
-def tokenize(s):
-    return {word.lower() for word in word_tokenize(s) if word.isalpha() and word not in stopwords}
-
-def index(courses):
-    for a_id, _, a_description in courses:
-        for b_id, _, b_description in courses:
-            yield a_id, b_id, jaccard_distance( a_description, b_description )
+def index(names, descriptions):
+    for i, a in enumerate(names):
+        for j, b in enumerate(names):
+            yield a, b, descriptions[i].similarity(descriptions[j])
 
 
 if __name__ == '__main__':
+
+    print("Reading CSV")
     with open('data/umd_courses.csv') as csvfile:
         reader = csv.reader(csvfile)
-        courses = [(id, name, tokenize(description)) for id, name, description in reader ]
+        courses = [(id, name, description) for id, name, description in reader]
 
-    lc = len(courses)
-    for i, (a_id, b_id, score) in enumerate(index(courses)):
-        print(a_id, b_id, score, f'{i}/{lc**2}')
+    courses = courses[:1000]
+
+    print("Loading model")
+    nlp = spacy.load('en_core_web_md')
+
+    print("Generating descriptions")
+    names = [course[0] for course in courses]
+    descriptions = [course[1] + course[2] for course in courses]
+    descriptions = [nlp(description) for description in descriptions]
+
+    print("Indexing")
+    for i, (a, b, score) in enumerate(index(names, descriptions)):
+        print(a, b, score, f'{i}/{len(names) ** 2}')
