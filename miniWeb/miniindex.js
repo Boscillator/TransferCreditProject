24cdeacfdf138b86916f6a3fb9bab442f7370665
Fred Buchanan <oscillator.b@gmail.com>
2019-11-05 13:08:31 -0500

    Integrated minindex.js

--- miniWeb/miniindex.js
+++ miniWeb/miniindex.js
@@ -1,17 +1,22 @@
+function makeLink(lab) {
+    return `<li><a href="./lab.html#${lab.FileName}">${lab.FileName}</a>`
+}
+
 $(document).ready(function() {
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
     var yyyy = today.getFullYear();
     today = yyyy + '-' + mm +  '-' + dd ;
-    $.getJSON('', function(data) {
-        for (var i = 0; i < data.length(); i++) {
+    $.getJSON('labs.json', function(data) {
+        for (var i = 0; i < data.length; i++) {
+            console.log(data[i]);
             if (data[i].DueDate == today) {
-                $('#present').append('<li>' + data[i].fileName + '</li>')
+                $('#present').append(makeLink(data[i]));
             } else if (data[i].DueDate > today) {
-                $('#future').append('<li>' + data[i].fileName + '</li>')
+                $('#future').append(makeLink(data[i]));
             } else if (data[i].DueDate < today) {
-                $('#past').append('<li>' + data[i].fileName + '</li>')
+                $('#past').append(makeLink(data[i]));
             }
         }
     })
