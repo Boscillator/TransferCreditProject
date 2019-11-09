1d9cefa6cbb6247117a31018610ee5643adb1435
Fred Buchanan <oscillator.b@gmail.com>
2019-11-09 12:26:06 -0500

    Added credentials file.

--- data_layer/connection.php
+++ data_layer/connection.php
@@ -1,10 +1,11 @@
 <?php
+include_once './credentials.php';
+
 function getConnection() {
-  $connection = new mysqli("localhost", "username", "password", "TransferCredit");
+  $connection = new mysqli(MYSQL_HOST, MYSQL_USERNAME, MYSQL_PASSWORD, "TransferCredit");
   if ($connection->connect_errno) {
     echo "Failed to connect " . $connection->connect_error;
   }
   // echo "Connection Successful";
   return $connection;
 }
-?>
