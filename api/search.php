86a0d26595a054cb3e58a891cada6286f34e9aec
Fred Buchanan <oscillator.b@gmail.com>
2019-11-10 15:51:20 -0500

    Documented controllers.

--- api/search.php
+++ api/search.php
@@ -1,8 +1,28 @@
 <?php
+
+/**
+ * Search for similar classes.
+ *
+ * Request:
+ *  GET api/search.php?id=1234
+ * Response:
+ *   [
+ *      {
+ *          "id": 2399,
+ *          "school": "HVCC",
+ *          "code": "HIST 115",
+ *          "course_name": "Introduction to African-American History",
+ *          "description": "A study of the developments and..."
+ *      }, ...
+ *  ]
+ */
+
 include_once(dirname(__FILE__).'/../data_layer/models/Course.php');
 
+// Tell JQUERY that this response is JSON
 header('Content-Type: application/json');
 
+// Load query parameters id and n. Use default of 10 if n is not in the query parameters
 $id = intval($_GET['id']);
 $n = 10;
 if(isset($_GET['n'])) {
